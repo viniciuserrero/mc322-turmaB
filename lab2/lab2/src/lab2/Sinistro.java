cd55b558c6ec4f77f61769a9895a4a3cd9a7fbdd
Vinicius Errero <vinnyerrero@gmail.com>
2023-03-27 18:24:07 -0300

    Laboratório 2 (#1)
    Essa PR inclui os arquivos do Laboratório 2 da Disciplina de MC322

---
+++ lab2/lab2/src/lab2/Sinistro.java
@@ -0,0 +1,43 @@
+package lab2;
+import java.util.UUID;
+
+public class Sinistro {
+    private int id ;
+    private String data ;
+    private String endereco ;
+
+    // Construtor
+    public Sinistro (String data , String endereco ) {
+        this.id = generate_id();
+        this.data = data ;
+        this.endereco = endereco ;
+    }
+
+    // Getters e setters
+    public int getID () {
+        return id ;
+    }
+
+    public String getData () {
+        return data ;
+    }
+
+    public void setData ( String data ) {
+        this.data = data ;
+    }
+
+    public String getEndereco () {
+        return endereco ;
+    }
+
+    public void setEndereco ( String endereco ) {
+        this.endereco = endereco ;
+    }
+
+    // Generate id
+    private int generate_id() {
+        UUID random_id = UUID.randomUUID();
+        return (random_id.hashCode() & Integer.MAX_VALUE);
+    }
+
+}
