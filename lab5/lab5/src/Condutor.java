f27b0c10101ec007c7f4392e3102104843b06fa5
Vinicius Errero <vinnyerrero@gmail.com>
2023-06-22 00:02:14 -0300

    add condutor and seguro classes

--- lab5/lab5/src/Condutor.java
+++ lab5/lab5/src/Condutor.java
@@ -1,5 +1,106 @@
 package lab5;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Condutor {
-  return null;
+    private final String cpf;
+    private String nome;
+    private String telefone;
+    private String endereco;
+    private String email;
+    private Date dataNascimento;
+    private ArrayList<Veiculo> listaSinistros;
+
+    public Condutor(
+            String cpf,
+            String nome,
+            String telefone,
+            String endereco,
+            String email,
+            Date dataNascimento,
+            ArrayList<Veiculo> listaSinistros) {
+
+        if(Validacao.validarCPF(cpf)){
+            this.cpf = cpf;
+        }
+        
+        else{
+            this.cpf = null;
+        }
+        
+        this.nome = nome;
+        this.telefone = telefone;
+        this.endereco = endereco;
+        this.email = email;
+        this.dataNascimento = dataNascimento;
+        this.listaSinistros = listaSinistros;
+
+    }
+    //Getters e setters
+    public String getCPF() {
+        return cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco){
+        this.endereco = endereco;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public ArrayList<Veiculo> getListaSinistros(){
+        return this.listaSinistros;
+    }
+
+    public void setListaSinistros(ArrayList<Veiculo> listaSinistros){
+        this.listaSinistros = listaSinistros;
+    }
+
+    public void addSinistro(Veiculo veiculo){
+        this.listaSinistros.add(veiculo);
+    }
+
+    public void removeSinistro(Veiculo veiculo){
+        this.listaSinistros.remove(veiculo);
+    }
+
+    public String toString() {
+        return "Condutor:\n cpf=" + cpf + ", nome=" + nome + ", telefone=" + telefone + ", endereco=" + endereco
+                + ", email=" + email + ", dataNascimento=" + dataNascimento + ", listaSinistros=" + listaSinistros
+                + "";
+    }
 }
--- lab5/lab5/src/Seguro.java
+++ lab5/lab5/src/Seguro.java
@@ -1,5 +1,109 @@
 package lab5;
 
+import java.util.Date;
+import java.util.UUID;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Seguro {
-    return null;
+    private final int id ;
+    private Date dataInicio ;
+    private Date dataFim ;
+    private Seguradora seguradora ;
+    private ArrayList<Veiculo> listaVeiculos;
+    private ArrayList<Condutor> listaCondutores;
+    private int valorMensal;
+
+    public Seguro(
+        Date dataInicio,
+        Date dataFim,
+        Seguradora seguradora,
+        List<Veiculo> listaVeiculos,
+        List<Condutor> listaCondutores,
+        int valorMensal ){
+            this.id = generateID();
+            this.dataInicio = dataInicio;
+            this.dataFim = dataFim;
+            this.seguradora = seguradora;
+            this.listaVeiculos = listaVeiculos;
+            this.listaCondutores = listaCondutores;
+            this.valorMensal = valorMensal;
+        }
+        
+        // Getters e setters
+        public int getID () {
+            return id ;
+        }
+
+        public Date getDataInicio () {
+            return dataInicio ;
+        }
+
+        public void setDataInicio ( Date dataInicio ) {
+            this.dataInicio = dataInicio ;
+        }
+
+        public Date getDataFim () {
+            return dataFim ;
+        }
+
+        public void setDataFim ( Date dataFim ) {
+            this.dataFim = dataFim ;
+        }
+
+        public Seguradora getSeguradora () {
+            return seguradora ;
+        }
+
+        public void setSeguradora ( Seguradora seguradora ) {
+            this.seguradora = seguradora ;
+        }
+
+        public List<Veiculo> getListaVeiculos () {
+            return listaVeiculos ;
+        }
+
+        public void setListaVeiculos ( List<Veiculo> listaVeiculos ) {
+            this.listaVeiculos = listaVeiculos ;
+        }
+
+        public List<Condutor> getListaCondutores () {
+            return listaCondutores ;
+        }
+
+        public void setListaCondutores ( List<Condutor> listaCondutores ) {
+            this.listaCondutores = listaCondutores ;
+        }
+
+        public int getValorMensal () {
+            return valorMensal ;
+        }
+
+        public void setValorMensal ( int valorMensal ) {
+            this.valorMensal = valorMensal ;
+        }
+
+        public void autozizarCondutor(Condutor condutor){
+            listaCondutores.add(condutor);
+        }
+
+        public void desautorizarCondutor(Condutor condutor){
+            listaCondutores.remove(condutor);
+        }
+
+        public int calcularValor(){
+            return 0;
+        }
+        // Generate id
+        private int generateID() {
+            UUID randomId = UUID.randomUUID();
+            return (randomId.hashCode() & Integer.MAX_VALUE);
+        }
+        //tostring
+        public String toString() {
+            return "Seguro:\ndataFim=" + dataFim + ", dataInicio=" + dataInicio + ", id=" + id + ", listaCondutores="
+                    + listaCondutores + ", listaVeiculos=" + listaVeiculos + ", seguradora=" + seguradora
+                    + ", valorMensal=" + valorMensal;
+        }
 }
